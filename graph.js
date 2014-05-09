7a7d93a7e1c4546a09ef81798789674f7357d612
Ilan <ilantch@gmail.com>
2014-05-10 01:58:52 +0300

    bugfix

--- graph.js
+++ graph.js
@@ -31,7 +31,7 @@ function nodesCtrl($scope) {
 
     $scope.updateCanvas = function(nodeId) {
         parentVal = $('#node' + nodeId).val();
-        if (parentVal == nodeId) {
+        if (parentVal === nodeId) {
             return;
         }
         if (parentVal == "") {
